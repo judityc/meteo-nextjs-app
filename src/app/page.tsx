c65c07cb779642ced4acb2e0d96757cff2585d6a
judityc <judityc@gmail.com>
2024-02-06 12:24:43 +0200

    Include Heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
 import styles from "./page.module.css";
 import { Box, Heading } from "@chakra-ui/react";
-import SelectCity from "../../components/SelectCity";import { useState } from "react";
-import error from "next/error";
+import SelectCity from "../../components/SelectCity";
+import { useState } from "react";
 import DataTable from "../../components/DataTable";
 import { useCityForecast } from "../../hooks/useCityForecast";
 
@@ -12,17 +12,12 @@ export default function Home() {
 
   const { data: cityForecast, error } = useCityForecast(city);
 
-
   return (
-      <Box
-      w="100%"
-      display="flex"
-      flexDirection="column"
-      gap={10}
-      alignItems="center"
-      padding={20}
-    >
-      <SelectCity onSelectCity={(cityCode) => setCity(cityCode)}/>
+    <Box className={styles.container}>
+      <Heading as="h2" size="2xl">
+        WEATHER APP
+      </Heading>
+      <SelectCity onSelectCity={(cityCode) => setCity(cityCode)} />
       {error ? <Heading>{error}</Heading> : <DataTable data={cityForecast} />}
     </Box>
   );
