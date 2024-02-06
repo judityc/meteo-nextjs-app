d444884afd66477b2975dd3dbbf74a9e195b35c5
judityc <judityc@gmail.com>
2024-02-06 13:11:56 +0200

    Implement dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import { Providers } from './providers'
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,13 +12,13 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode,
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
         <Providers>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
