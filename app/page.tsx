8a696e8fa800db72d7eebad857b0ef18b1409501
judityc <judityc@gmail.com>
2024-02-07 22:33:18 +0200

    implement search params support

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,20 @@ import SelectCity from "../components/SelectCity";
 import { useState } from "react";
 import DataTable from "../components/DataTable";
 import { useCityForecast } from "../hooks/useCityForecast";
-import DataTableSkeleton from "../components/DataTableSkeleton";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export default function Home() {
-  const [city, setCity] = useState("vilnius");
+  const router = useRouter();
+  const params = useSearchParams();
+  const cityCode = params?.get("city_code");
 
+  const [city, setCity] = useState(cityCode ?? "vilnius");
   const { data: cityForecast, error, isLoading } = useCityForecast(city);
+
+  const handleSelectCity = (cityCode: string) => {
+    setCity(cityCode);
+    router.push(`/?city_code=${cityCode}`);
+  };
 
   return (
     <>
@@ -19,7 +27,7 @@ export default function Home() {
         <Heading as="h2" size="2xl">
           WEATHER APP
         </Heading>
-        <SelectCity onSelectCity={(cityCode) => setCity(cityCode)} />
+        <SelectCity onSelectCity={handleSelectCity} />
         <DataTable data={cityForecast} error={error} isLoading={isLoading} />
       </Box>
     </>
--- components/DataTableSkeleton.tsx
+++ components/DataTableSkeleton.tsx
@@ -7,57 +7,56 @@ import {
   Tbody,
   Skeleton,
   Td,
-  Tfoot,
 } from "@chakra-ui/react";
 
 const DataTableSkeleton = () => {
-  const skeletons = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10];
+  const skeletons = Array(11).fill(0);
 
   return (
     <>
-    <TableContainer width="1110px" height="621px">
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th width="213px">Date And Time</Th>
-            <Th isNumeric>Temperature</Th>
-            <Th isNumeric>Feel Temperature</Th>
-            <Th isNumeric>Wind Speed</Th>
-            <Th isNumeric>Wind Direction</Th>
-            <Th isNumeric>Cloud Cover</Th>
-            <Th>Conditions</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {skeletons.map((skeleton) => (
-            <Tr key={skeleton}>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
-              <Td>
-                <Skeleton height="25px" />
-              </Td>
+      <TableContainer width="1110px" height="621px">
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th width="213px">Date And Time</Th>
+              <Th isNumeric>Temperature</Th>
+              <Th isNumeric>Feel Temperature</Th>
+              <Th isNumeric>Wind Speed</Th>
+              <Th isNumeric>Wind Direction</Th>
+              <Th isNumeric>Cloud Cover</Th>
+              <Th>Conditions</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  </>
+          </Thead>
+          <Tbody>
+            {skeletons.map((_, index) => (
+              <Tr key={index}>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+                <Td>
+                  <Skeleton height="25px" />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
