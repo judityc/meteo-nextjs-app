d0467a0dc33ec1c7ad8b0d04861082f018d3bf0a
judityc <judityc@gmail.com>
2024-02-07 22:46:23 +0200

    add suspense to providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,13 @@
 
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
+import { Suspense } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}><ColorModeScript initialColorMode={theme.config.initialColorMode}/>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <Suspense>{children}</Suspense>
+    </ChakraProvider>
+  );
 }
